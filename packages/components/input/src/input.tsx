9d4b0ebf1976e6c40d5df8b8d3dc97a166ca5fd5
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-05-15 18:06:04 +0700

    refactor(input): fix code structure

--- packages/components/input/src/input.tsx
+++ packages/components/input/src/input.tsx
@@ -11,12 +11,8 @@ const Input: React.FC<InputProps> = (props) => {
   const [passwordSeen, setPasswordSeen] = React.useState<boolean>(false);
   const [selectOpen, setSelectOpen] = React.useState<boolean>(false);
   const [searchTerm, setSearchTerm] = React.useState<string>("");
-  const [optionsPosition, setOptionsPosition] = React.useState<
-    "above" | "below"
-  >("below");
-  const [selectedOption, setSelectedOption] = React.useState<string | number>(
-    input.value
-  );
+  const [optionsPosition, setOptionsPosition] = React.useState<"above" | "below">("below");
+  const [selectedOption, setSelectedOption] = React.useState<string | number>(input.value);
   const ref = React.useRef<HTMLDivElement>(null);
   const optionsRef = React.useRef<HTMLDivElement>(null);
 
@@ -32,19 +28,9 @@ const Input: React.FC<InputProps> = (props) => {
             id={input.id}
             form={input.formId}
             className={s.inputFieldInput}
-            type={
-              input.type === "password"
-                ? passwordSeen
-                  ? "text"
-                  : "password"
-                : input.type
-            }
+            type={input.type === "password" ? (passwordSeen ? "text" : "password") : input.type}
             name={input.name}
-            placeholder={
-              input.isReadonly && input.value === ""
-                ? input.fallbackValue
-                : input.placeholder
-            }
+            placeholder={input.isReadonly && input.value === "" ? input.fallbackValue : input.placeholder}
             value={input.value}
             onChange={input.onChange}
             autoComplete={input.autoComplete}
@@ -59,11 +45,7 @@ const Input: React.FC<InputProps> = (props) => {
         );
       case "select":
         const filteredOptions =
-          searchTerm.length > 0
-            ? input.options.filter((option) =>
-                option.label.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-            : input.options;
+          searchTerm.length > 0 ? input.options.filter((option) => option.label.toLowerCase().includes(searchTerm.toLowerCase())) : input.options;
 
         const handleSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
           setSelectedOption(e.target.value);
@@ -81,16 +63,12 @@ const Input: React.FC<InputProps> = (props) => {
             <input
               id={input.id}
               form={input.formId}
-              className={`${s.inputFieldInput} ${
-                input.variant === "select" ? s.select : ""
-              }`}
+              className={`${s.inputFieldInput} ${input.variant === "select" ? s.select : ""}`}
               name={input.name}
               value={
                 selectedOption === ""
                   ? input.placeholder
-                  : input.options.find(
-                      (option) => option.value === selectedOption
-                    )?.label || input.placeholder
+                  : input.options.find((option) => option.value === selectedOption)?.label || input.placeholder
               }
               onChange={handleSelectChange}
               required={input.isRequired}
@@ -100,9 +78,7 @@ const Input: React.FC<InputProps> = (props) => {
             />
             {selectOpen && (
               <div
-                className={`${s.optionsContainer} ${
-                  selectOpen ? s.opened : s.closed
-                } ${optionsPosition === "above" ? s.above : s.below}`}
+                className={`${s.optionsContainer} ${selectOpen ? s.opened : s.closed} ${optionsPosition === "above" ? s.above : s.below}`}
                 ref={optionsRef}
               >
                 {input.isSearchable ? (
@@ -115,9 +91,7 @@ const Input: React.FC<InputProps> = (props) => {
                   />
                 ) : (
                   <div
-                    className={`${s.option} ${
-                      selectedOption === "" ? s.selected : ""
-                    }`}
+                    className={`${s.option} ${selectedOption === "" ? s.selected : ""}`}
                     onClick={() => {
                       setSelectOpen(false);
                       setSelectedOption("");
@@ -129,9 +103,7 @@ const Input: React.FC<InputProps> = (props) => {
                 {filteredOptions.map((option) => (
                   <div
                     key={option.value}
-                    className={`${s.option} ${
-                      selectedOption === option.value ? s.selected : ""
-                    }`}
+                    className={`${s.option} ${selectedOption === option.value ? s.selected : ""}`}
                     onClick={() => handleOptionClick(option.value)}
                   >
                     <b className={s.optionText}>{option.label}</b>
@@ -148,11 +120,7 @@ const Input: React.FC<InputProps> = (props) => {
             form={input.formId}
             className={s.inputFieldInput}
             name={input.name}
-            placeholder={
-              input.isReadonly && input.value === ""
-                ? input.fallbackValue
-                : input.placeholder
-            }
+            placeholder={input.isReadonly && input.value === "" ? input.fallbackValue : input.placeholder}
             value={input.value}
             onChange={input.onChange}
             autoComplete={input.autoComplete}
@@ -170,12 +138,7 @@ const Input: React.FC<InputProps> = (props) => {
   };
 
   useMousedown((event: MouseEvent) => {
-    if (
-      ref.current &&
-      !ref.current.contains(event.target as Node) &&
-      optionsRef.current &&
-      !optionsRef.current.contains(event.target as Node)
-    ) {
+    if (ref.current && !ref.current.contains(event.target as Node) && optionsRef.current && !optionsRef.current.contains(event.target as Node)) {
       setSelectOpen(false);
     }
   }, []);
@@ -205,28 +168,17 @@ const Input: React.FC<InputProps> = (props) => {
       secondaryColor={input.secondaryColor}
       className={s.inputBody}
     >
-      <label
-        htmlFor={input.id}
-        className={`${s.inputLabel} ${
-          input.isLabeled && input.labelText ? "" : s.none
-        }`}
-      >
+      <label htmlFor={input.id} className={`${s.inputLabel} ${input.isLabeled && input.labelText ? "" : s.none}`}>
         {`${input.labelText} ${input.isRequired ? "*" : ""}`}
       </label>
       <div
-        className={`${s.inputField} ${input.errorContent ? s.error : ""} ${
-          input.isReadonly ? s.readonly : ""
-        } ${input.isDisabled ? s.disabled : ""} ${
-          input.variant !== "textarea" ? s.plain : ""
-        } ${input.variant !== "select" ? s.inputVariant : ""}`}
+        className={`${s.inputField} ${input.errorContent ? s.error : ""} ${input.isReadonly ? s.readonly : ""} ${
+          input.isDisabled ? s.disabled : ""
+        } ${input.variant !== "textarea" ? s.plain : ""} ${input.variant !== "select" ? s.inputVariant : ""}`}
         ref={input.variant === "select" ? ref : undefined}
         style={getInputStyles(input.radius)}
       >
-        {input.startContent && (
-          <div className={`${s.inputIcon} ${s.start}`}>
-            {input.startContent}
-          </div>
-        )}
+        {input.startContent && <div className={`${s.inputIcon} ${s.start}`}>{input.startContent}</div>}
         {renderVariant()}
         {input.variant === "input" && input.type === "password" && (
           <div
@@ -237,41 +189,24 @@ const Input: React.FC<InputProps> = (props) => {
             tabIndex={0}
           >
             {passwordSeen ? (
-              <ISEyeSlash
-                width="100%"
-                height="100%"
-                color={input.secondaryColor}
-              />
+              <ISEyeSlash width="100%" height="100%" color={input.secondaryColor} />
             ) : (
-              <ISEyeOpen
-                width="100%"
-                height="100%"
-                color={input.secondaryColor}
-              />
+              <ISEyeOpen width="100%" height="100%" color={input.secondaryColor} />
             )}
           </div>
         )}
         {input.variant === "select" && (
           <div
-            className={`${s.inputIcon} ${s.select} ${
-              selectOpen ? s.flipped : ""
-            }`}
+            className={`${s.inputIcon} ${s.select} ${selectOpen ? s.flipped : ""}`}
             aria-label={selectOpen ? "Close Option" : "Open Option"}
             tabIndex={0}
           >
-            <ISChevron
-              width="100%"
-              height="100%"
-              direction="down"
-              color={input.secondaryColor}
-            />
+            <ISChevron width="100%" height="100%" direction="down" color={input.secondaryColor} />
           </div>
         )}
-        {input.variant === "input" &&
-          input.type !== "password" &&
-          input.endContent && (
-            <div className={`${s.inputIcon} ${s.end}`}>{input.endContent}</div>
-          )}
+        {input.variant === "input" && input.type !== "password" && input.endContent && (
+          <div className={`${s.inputIcon} ${s.end}`}>{input.endContent}</div>
+        )}
       </div>
       {input.errorContent && (
         <h6 className={s.inputInfoText} style={{ color: "#FF6347" }}>
